1584138601047707bdac076ec8b9340fd6feed70
nghiadv <daonghiabk@gmail.com>
2024-08-04 10:32:52 +0700

    add modular

---
+++ modular/repositories/todo_repository.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+from typing import List, Union
+
+class TodoCreate(BaseModel):
+    title: str
+
+class Todo(TodoCreate):
+    id: int
+    completed: bool = False
+
+class TodoRepository:
+    def __init__(self):
+        self.todos = []
+
+    def create_todo(self, todo: TodoCreate) -> Todo:
+        new_todo = Todo(id=len(self.todos) + 1, **todo.model_dump())
+        self.todos.append(new_todo)
+        return new_todo
+
+    def get_todos(self) -> List[Todo]:
+        return self.todos
+
+    def get_todo(self, todo_id: int) -> Union[Todo, None]:
+        for todo in self.todos:
+            if todo.id == todo_id:
+                return todo
+        return None
+
+    def update_todo(self, todo_id: int, updated_todo: TodoCreate) -> Union[Todo, None]:
+        for todo in self.todos:
+            if todo.id == todo_id:
+                todo.title = updated_todo.title
+                return todo
+        return None
+
+    def delete_todo(self, todo_id: int) -> bool:
+        for index, todo in enumerate(self.todos):
+            if todo.id == todo_id:
+                del self.todos[index]
+                return True
+        return False
